7358d0bb093ca71fbda44357ad73f389b1d89973
Richard Melvin <rgmelvin@pop-os.localdomain>
2025-04-27 18:02:13 -0500

    Fix ESLint config for correct TypeScript + Browser globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,27 @@
 import js from "@eslint/js";
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import parser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 import { defineConfig } from "eslint/config";
-
+import ts from "typescript";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  {
+    files:["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      js,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...ts.configs.recommended.rules,
+    },
+  },
 ]);
